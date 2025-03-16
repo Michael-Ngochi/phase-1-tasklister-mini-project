cfcffa5964fb881851b2b9b650c6555d89eac03d
Michael-Ngochi <ngoochimike@gmail.com>
2025-03-16 15:11:41 +0300

    added list fuctionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const taskList = document.querySelector("#tasks");
+  const form = document.querySelector("#create-task-form");
+  const formInput = document.querySelector("#new-task-description");
+
+  let taskArray = [];
+
+  form.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    if (formInput.value.trim() !== "") {
+      taskArray.push(formInput.value.trim());
+      showTasks();
+      formInput.value = ""; 
+    }
+  });
+
+  function showTasks() {
+    taskList.innerHTML = "";
+    taskArray.forEach((task) => {
+      let li = document.createElement("li");
+      li.textContent = task;
+      taskList.appendChild(li);
+    });
+  }
+
+  showTasks();
 });
